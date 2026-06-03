8ce818839d586ff9b21c19161100ff1610ff9bcc
vavinon <34766650+vavinon@users.noreply.github.com>
2026-06-03 04:59:26 +0000

    Deploying to gh-pages from @ vavinon/bmi_calculator@c6b57f1f7e895d014453b2c0426c3e6c6467f558 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "339301707" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "215183895" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
